d87522236cc86452f363efc509a2a11492375911
Alp Yaltı <45272167+alpyalti@users.noreply.github.com>
2023-10-09 15:42:32 +0300

    Character Animation Added

--- Assets/Scripts/CharacterScripts/PlayerMovement.cs
+++ Assets/Scripts/CharacterScripts/PlayerMovement.cs
@@ -7,12 +7,19 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
     Vector2 movement;
+    public Animator animator;
     
     // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        
+        movement.Normalize();
+        
+        animator.SetFloat(("Horizontal"), movement.x);
+        animator.SetFloat(("Vertical"), movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     void FixedUpdate()
