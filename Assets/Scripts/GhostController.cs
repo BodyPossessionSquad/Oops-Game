7bacbb022cd628ef694e7e2c8f483c665f3eadc2
MrNeal01 <nailturkoglu1999@gmail.com>
2023-11-08 12:36:18 +0200

    bug fix

--- Assets/Scripts/GhostController.cs
+++ Assets/Scripts/GhostController.cs
@@ -15,6 +15,7 @@ public class GhostController : MonoBehaviour
     public Animator animator;
 
     private CameraFollow cameraFollow;
+    private bool isPossessing = false;
 
     public static GhostController Instance { get; private set; }
 
@@ -40,6 +41,12 @@ public class GhostController : MonoBehaviour
 
     void Update()
     {
+         if (isPossessing)
+        {
+            // If we're currently in the process of possessing, ignore input
+            return;
+        }
+
         if (currentPossessedCharacter != null)
         {
             transform.position = currentPossessedCharacter.transform.position;
@@ -68,6 +75,7 @@ public class GhostController : MonoBehaviour
 
     void PossessCharacter(GameObject character)
     {
+        isPossessing = true;
         // Start possession animation immediately
         animator.SetBool("isPossessing", true);
 
@@ -112,6 +120,7 @@ public class GhostController : MonoBehaviour
                 CoroutineManager.Instance.ExecuteAfterDelay(possessionTime, DepossessCharacter, "Depossess");
             }
 
+            isPossessing = false;
             HealGhostFixed();
         }, "Possession");
     }
@@ -119,6 +128,11 @@ public class GhostController : MonoBehaviour
     public void DepossessCharacter()
 {
     Debug.Log("Depossessing character");
+
+     if (isPossessing)
+     {
+        return;
+     }
 
     // Check if there is a character to depossess
     if (currentPossessedCharacter != null)
