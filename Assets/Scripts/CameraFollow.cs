5e975ab7b3ae5c64f0dc0124e4757a9c99d332c2
MrNeal01 <nailturkoglu1999@gmail.com>
2023-11-27 17:00:39 +0200

    camera

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,6 +6,22 @@ public class CameraFollow : MonoBehaviour
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
 
+    private float minX, maxX, minY, maxY;
+
+    private void Start()
+    {
+        // Automatically find the EdgeCollider2D in the scene
+        EdgeCollider2D boundaryCollider = FindObjectOfType<EdgeCollider2D>();
+        if (boundaryCollider != null)
+        {
+            CalculateBounds(boundaryCollider);
+        }
+        else
+        {
+            Debug.LogError("EdgeCollider2D not found in the scene.");
+        }
+    }
+
     public void SetTarget(Transform newTarget)
     {
         target = newTarget;
@@ -22,7 +38,22 @@ public class CameraFollow : MonoBehaviour
         {
             Vector3 desiredPosition = target.position + offset;
             Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+            // Clamp the camera position to the calculated boundaries
+            smoothedPosition.x = Mathf.Clamp(smoothedPosition.x, minX, maxX);
+            smoothedPosition.y = Mathf.Clamp(smoothedPosition.y, minY, maxY);
             transform.position = smoothedPosition;
         }
+    }
+
+    void CalculateBounds(EdgeCollider2D boundaryCollider)
+    {
+        Bounds boundary = boundaryCollider.bounds;
+        float halfHeight = Camera.main.orthographicSize;
+        float halfWidth = Camera.main.aspect * halfHeight;
+
+        minX = boundary.min.x + halfWidth;
+        maxX = boundary.max.x - halfWidth;
+        minY = boundary.min.y + halfHeight;
+        maxY = boundary.max.y - halfHeight;
     }
 }
